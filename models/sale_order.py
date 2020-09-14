4c8a5c355e77aff013dda22a17542f0a3afc3c48
lizeth-miranda <67765437+lizeth-miranda@users.noreply.github.com>
2020-09-14 11:06:49 -0600

    Add files via upload

---
+++ models/sale_order.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+# instruccion para hacer importaciones desde odoo
+from odoo import api, fields, models
+
+
+class Cuenta(models.Model):
+    _inherit = 'sale.order'
+
+    analytic_account_id = fields.Many2one(
+        required=True,
+    )

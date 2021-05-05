dee4f99134921b1d8cb7e708163d2cfb9cd06e81
lizeth-miranda <67765437+lizeth-miranda@users.noreply.github.com>
2021-05-05 10:32:54 -0600

    Update hr_leave.py

--- models/hr_leave.py
+++ models/hr_leave.py
@@ -8,17 +8,14 @@ class hr_lea(models.Model):
 
     account_ids = fields.Many2one(
         comodel_name='account.analytic.account',
-        string="Project",
+        string="Obra",
         required=True,
     )
     cost_day = fields.Monetary(
         related='employee_id.cost_day',
         string="Cost day",
     )
-    cost_default = fields.Monetary(
-        compute='_cost_default',
-        string="Costo/Falta",
-    )
+
     currency_id = fields.Many2one(
         related='employee_id.currency_id',
     )
@@ -26,21 +23,23 @@ class hr_lea(models.Model):
         related="employee_id.department_id.name",
         string="Puesto de Trabajo",
     )
+    num_emp = fields.Char(related="employee_id.cod_emp",)
 
-    @api.depends('number_of_days')
-    def _cost_default(self):
-        for record in self:
-            record.cost_default = (
-                record.cost_day * record.number_of_days) * -1
+    leavee = fields.Boolean(
+        string="Falta", default=True,
+    )
+
 
     def action_approve(self):
         res = super(hr_lea, self).action_approve()
-        self.env['account.analytic.line'].create({
-            'date': self.request_date_from,
-            'name': self.employee_id.name,
-            'job_pos': self.depa,
-            'account_id': self.account_ids.id,
-            'amount': self.cost_default,
+        self.env['nomina.line'].create({
+            'employee_id': self.employee_id.id,
+            'codigo_empleado': self.num_emp,
+            'project': self.account_ids.id,
+            'department': self.pues_tra,
+            'fecha': self.request_date_from,
+            'inci': self.holiday_status_id.name,
+            'leavee': self.leavee,
+            # 'total_inci': self.cost_default,
         })
         return res
-

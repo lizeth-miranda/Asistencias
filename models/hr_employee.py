3e20428020912d524e486660ae32a7c5b2860e1f
lizeth-miranda <67765437+lizeth-miranda@users.noreply.github.com>
2021-07-13 09:14:35 -0600

    Update hr_employee.py

--- models/hr_employee.py
+++ models/hr_employee.py
@@ -65,85 +65,22 @@ class empl(models.Model):
         ('enterprise', 'PCA Grupo Prefabricador'),
         ('enterprise2', 'DEMSA'),
     ], string="Empresa",)
+    
+    discounts_ids = fields.One2many(
+        comodel_name='discount.employee',
+        inverse_name='employee',
+    )
 
-    # calcular prestamos
-    fecha_actual = fields.Datetime(
-        string="Fecha Préstamo", default=fields.Datetime.now, readonly=False,)
-    total_prestamo = fields.Monetary(string="Préstamo",)
-    semanas = fields.Float(string="Semanas",)
-    abono = fields.Monetary(
-        string="Abono", compute="compute_abono", store=True, )
-    fecha_pp = fields.Datetime(
-        compute="compute_fecha_pp", string="Fecha Primer Pago",)
-    onlyfecha_pp = fields.Date(compute="compute_onlyfecha_pp")
-    fecha_up = fields.Datetime(
-        compute="compute_fecha_up", string="Fecha Final de pago",)
+    pres_perso = fields.Monetary(
+        string="Préstamo Personal", related="discounts_ids.sum_abono",)
 
-    # calcular solo fecha de el primer pago
-    @ api.depends('fecha_pp')
-    def compute_onlyfecha_pp(self):
-        for record in self:
-            f1 = record.fecha_pp
-            date = fields.Datetime.to_string(fields.Datetime.context_timestamp(
-                self, fields.Datetime.from_string(f1)))[:10]
-            record.onlyfecha_pp = date
+    desc_HPP = fields.Monetary(
+        string="Dec.EPP", related="discounts_ids.sum_descEPP",)
 
-    @ api.depends('semanas')
-    def compute_abono(self):
-        for record in self:
-            if not record.semanas:
-                record.abono = 0.0
-            else:
-                record.abono = record.total_prestamo / record.semanas
+    otros_desc = fields.Monetary(
+        string="Otros Descuentos", related="discounts_ids.sum_otros_desc",)
 
-    # esta función le suma dias,horas, minutos, etc a un campo Datetime**
-    @ api.depends('semanas')
-    def compute_fecha_pp(self):
-        self.fecha_pp = self.fecha_actual + timedelta(weeks=1)
-        return fields.Date.context_today(self, timestamp=self.fecha_pp)
-
-    # esta función le suma dias,horas, minutos, etc a un campo Datetime**
-    @ api.depends('semanas')
-    def compute_fecha_up(self):
-        self.fecha_up = self.fecha_actual + timedelta(weeks=(self.semanas))
-        return fields.Date.context_today(self, timestamp=self.fecha_up)
-
-    # Descuentos personales
-    fecha_descuento = fields.Datetime(
-        string="Fecha", default=fields.Datetime.now, readonly=False,)
-    descuento = fields.Monetary(string="Total material a cobro",)
-    rango = fields.Float(string="Num Semanas",)
-    pago = fields.Monetary(string="Cantidad a Descontar", compute="compute_pago", store=True,)
-    fecha_pd = fields.Datetime(
-        compute="compute_fecha_pd", string="Fecha Primer Descuento",)
-    onlyfecha_pd = fields.Date(compute="compute_onlyfecha_pd")
-    fecha_final = fields.Datetime(
-        compute="compute_fecha_final", string="Fecha Final",)
-    desc = fields.Text(string="Descripción",)
-
-    # calcular solo fecha de el primer pago
-    @ api.depends('fecha_pd')
-    def compute_onlyfecha_pd(self):
-        for record in self:
-            f2 = record.fecha_pd
-            date2 = fields.Datetime.to_string(fields.Datetime.context_timestamp(
-                self, fields.Datetime.from_string(f2)))[:10]
-            record.onlyfecha_pd = date2
-
-    @api.depends('rango')
-    def compute_pago(self):
-        for record in self:
-            if not record.rango:
-                record.pago = 0.0
-            else:
-                record.pago = record.descuento / record.rango
-
-    @api.model  # esta función le suma dias,horas, minutos, etc a un campo Datetime**
-    def compute_fecha_pd(self):
-        self.fecha_pd = self.fecha_descuento + timedelta(weeks=1)
-        return fields.Date.context_today(self, timestamp=self.fecha_pd)
-
-    @api.depends('rango')
-    def compute_fecha_final(self):
-        self.fecha_final = self.fecha_descuento + timedelta(weeks=(self.rango))
-        return fields.Date.context_today(self, timestamp=self.fecha_final)
+    depo = fields.Monetary(related="discounts_ids.deposito",)
+    
+    
+   

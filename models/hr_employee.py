9b64eba2c9b5080328715b7ca5d2128f97f2581b
lizeth-miranda <67765437+lizeth-miranda@users.noreply.github.com>
2021-08-02 10:27:27 -0600

    Update hr_employee.py

--- models/hr_employee.py
+++ models/hr_employee.py
@@ -1,4 +1,4 @@
-  # -*- coding: utf-8 -*-
+# -*- coding: utf-8 -*-
 # instruccion para hacer importaciones desde odoo
 from odoo import fields, models, api
 from datetime import datetime, timedelta
@@ -16,19 +16,18 @@ class empl(models.Model):
     afiliacion_imss = fields.Char(string="Afiliación IMSS",)
 
     cost_day = fields.Monetary(
-        help="sueldo semanal/6",
-        string="Costo/Día",
-    )
+        help="sueldo semanal/6", string="Costo/Día", compute="compute_costday",)
+
     costo_dayCS = fields.Monetary(
         string="Carga Social por Día",
     )
 
     timesheet_cost = fields.Monetary(
-        'Costo por Hora'
-    )
+        'Costo por Hora', compute="compute_timecost", help="(sueldo/ 6) /8",)
     cost_extra = fields.Monetary(
-        help="Costo Extra/hr",
+        help="(sueldo/6) /8 *2",
         string="Costo Extra",
+        compute="compute_costExtra",
     )
     cost_default = fields.Monetary(
         help="cost per absence",
@@ -65,7 +64,7 @@ class empl(models.Model):
         ('enterprise', 'PCA Grupo Prefabricador'),
         ('enterprise2', 'DEMSA'),
     ], string="Empresa",)
-    
+
     discounts_ids = fields.One2many(
         comodel_name='discount.employee',
         inverse_name='employee',
@@ -75,13 +74,31 @@ class empl(models.Model):
         string="Préstamo Personal", related="discounts_ids.sum_abono",)
 
     desc_HPP = fields.Monetary(
-        string="Dec.EPP", related="discounts_ids.sum_descEPP",)
+        string="Desc.EPP", related="discounts_ids.sum_descEPP",)
 
     otros_desc = fields.Monetary(
         string="Otros Descuentos", related="discounts_ids.sum_otros_desc",)
 
     depo = fields.Monetary(related="discounts_ids.deposito",)
-    
+
     fecha_nacimiento = fields.Date(string="Fecha de Nacimiento",)
+
+    @api.depends('salary')
+    def compute_timecost(self):
+        for rec in self:
+            rec.timesheet_cost = (rec.salary/6) / 8
+
+    @api.depends('salary')
+    def compute_costday(self):
+        for rec in self:
+            rec.cost_day = rec.salary/6
+
+    @api.depends('salary')
+    def compute_costExtra(self):
+        for rec in self:
+            r1 = (rec.salary/6)
+            r2 = r1 / 8
+            r3 = r2 * 2
+            rec.cost_extra = r3
 
    

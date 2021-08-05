6def85f45cdfdc1f86565e8d84ee6cd7b2e94776
lizeth-miranda <67765437+lizeth-miranda@users.noreply.github.com>
2021-08-05 09:18:27 -0600

    Update nomina.py

--- models/nomina.py
+++ models/nomina.py
@@ -390,14 +390,15 @@ class Nomina(models.Model):
     @api.depends('cost_day', 'cant_asis', 'cant_ausen')
     def compute_suel_sem_faltas(self):
         for rec in self:
-            if rec.cant_asis >= 4 and rec.cant_ausen > 0 :
+            m1 = rec.cant_asis + rec.cant_ausen
+            if m1 < 6:
+                r1 = rec.extra_cost * rec.cant_ausen
+                rec.suel_Sem_faltas = (
+                    rec.cost_day * rec.cant_asis + rec.cost_day) - r1
+            elif m1 == 6:
                 r1 = rec.extra_cost * rec.cant_ausen
                 rec.suel_Sem_faltas = (
                     rec.cost_day * rec.cant_asis) - r1     
-            else:
-                r1 = rec.extra_cost * rec.cant_ausen
-                rec.suel_Sem_faltas = (
-                    rec.cost_day * rec.cant_asis + rec.cost_day) - r1     
 
     # calcular sueldo con nuevo ingreso
     suel_nuevo_ingreso = fields.Monetary(

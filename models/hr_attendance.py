521d0879156edff21804a8eb8a35fbeb404a2ece
lizeth-miranda <67765437+lizeth-miranda@users.noreply.github.com>
2021-09-21 12:02:43 -0600

    Update hr_attendance.py

--- models/hr_attendance.py
+++ models/hr_attendance.py
@@ -3,9 +3,6 @@
 from odoo import api, fields, models, exceptions, _
 from datetime import date, datetime, timedelta
 from odoo.exceptions import ValidationError, UserError
-# import time
-# import pytz
-
 
 class hr_atten(models.Model):
     _inherit = 'hr.attendance'
@@ -18,9 +15,10 @@ class hr_atten(models.Model):
     #     related="employee_id.department_id",
     #     string="Puesto",
     # )
-    check_out = fields.Datetime(
-        string="Check Out", )
-    #default=fields.Datetime.now,
+    # check_in = fields.Datetime(
+    #     default=fields.Datetime.now,)
+    # check_out = fields.Datetime(
+    #     string="Check Out", default=fields.Datetime.now,)
     fecha = fields.Date(string="Fecha",
                         required=True, readonly=False, default=fields.Date.today)
     hora_in = fields.Float(string="Entrada",)
@@ -50,11 +48,8 @@ class hr_atten(models.Model):
     )
     hours = fields.Float(
         related="employee_id.horas_lab",
-        # related="employee_id.resource_calendar_id.attendance_ids.hours",
         string="Horas Laborales",
     )
-    # hrs_lab_in = fields.Float(related="employee_id.horas_lab_in",)
-
     department = fields.Char(
         related="employee_id.department_id.name",
         string="Departamento",
@@ -88,30 +83,15 @@ class hr_atten(models.Model):
     tipo_resid = fields.Selection(related="user_id.tipo_resi")
 
     asistencia = fields.Boolean(default=True, string="asistencia",)
-    block_lines = fields.Selection([('done', 'Registrado')], string='Estado',)
 
-    @ api.depends('hours')
-    def _mitad(self):
-        for record in self:
-            if record.day == 5:
-                record.mitad = record.hours_sat/2
-            else:
-                record.mitad = record.hours / 2
+    block_lines = fields.Selection([('done', 'Registrado')], string='Estado', )
 
+  
     @ api.depends('fecha')
     def _day(self):
         for record in self:
             record.day = record.fecha.weekday()
 
-    # suma los valores de un campo de todos sus registros
-
-    @ api.depends('hora_in', 'hora_out')
-    def horas_traba(self):
-        for rec in self:
-            if rec.hora_out:
-                rec.horas_trab = rec.hora_out-rec.hora_in
-            else:
-                rec.horas_trab = False
 
     @ api.depends('fecha')
     def _total_hours(self):
@@ -120,7 +100,7 @@ class hr_atten(models.Model):
                 ('employee_id', '=', attendance.employee_id.name),
                 # ('account_ids', '=', attendance.account_ids.id),
                 ('fecha', '=', attendance.fecha),
-            ]).mapped('horas_trab'))
+            ]).mapped('worked_hours'))
 
     @ api.depends('total_hours')
     def _hours_extra(self):
@@ -139,40 +119,8 @@ class hr_atten(models.Model):
             elif attendance.day == 6:
                 attendance.hours_extra = attendance.total_hours
 
-    @ api.constrains('hora_in', 'employee_id')
-    def take(self):
-        for attendance in self:
-            last_attendance_before_check_out = self.env['hr.attendance'].search([
-                ('employee_id', '=', attendance.employee_id.id),
-                ('account_ids', '!=', attendance.account_ids.id),
-                ('fecha', '=', attendance.fecha),
-                ('hora_in', '<=', attendance.hora_in),
-                ('hora_out', '>=', attendance.hora_in),
-                ('id', '!=', attendance.id),
-            ]).mapped('account_ids')
-            print(last_attendance_before_check_out)
-            if last_attendance_before_check_out:
-                raise ValidationError(_("No se puede crear un nuevo registro de asistencia para el empleado %(empl_name)s, el empleado ya cuenta con una asistencias registrada en la hora de entrada que esta tratando de ingresar") % {
-                    'empl_name': attendance.employee_id.name,
-                    # 'datetime': fields.Datetime.to_string(fields.Datetime.context_timestamp(self, fields.Datetime.from_string(attendance.check_in_kiosko))),
-                })
 
-            last_attendance_before_check_out2 = self.env['hr.attendance'].search([
-                ('employee_id', '=', attendance.employee_id.id),
-                ('account_ids', '=', attendance.account_ids.id),
-                ('fecha', '=', attendance.fecha),
-                ('hora_in', '<=', attendance.hora_in),
-                ('hora_out', '>=', attendance.hora_in),
-                ('id', '!=', attendance.id),
-            ]).mapped('account_ids')
-            print(last_attendance_before_check_out2)
-            if last_attendance_before_check_out2:
-                raise ValidationError(_("No se puede crear un nuevo registro de asistencia para el empleado %(empl_name)s, el empleado ya cuenta con una asistencias registrada en la hora de entrada que esta tratando de ingresar") % {
-                    'empl_name': attendance.employee_id.name,
-                    # 'datetime': fields.Datetime.to_string(fields.Datetime.context_timestamp(self, fields.Datetime.from_string(attendance.check_in_kiosko))),
-                })
-
-            # create a new line, as none existed before
+    # create a new line, as none existed before
 
     @ api.constrains('fechaA')
     def nomina_line(self):
@@ -181,8 +129,8 @@ class hr_atten(models.Model):
                 ('employee_id.id', '=', record.employee_id.id),
                 ('project', '=', record.account_ids.id),
                 ('fechaA', '=', record.fecha),
-                ('check_in', '=', record.hora_in),
-                ('check_out', '=', record.hora_out),
+                ('check_in', '=', record.check_in),
+                ('check_out', '=', record.check_out),
             ])
             print(nomina_line)
             if nomina_line > 0:
@@ -195,9 +143,9 @@ class hr_atten(models.Model):
                     'department': record.department,
                     'project': record.account_ids.id,
                     'fechaA': record.fecha,
-                    'check_in': record.hora_in,
-                    'check_out': record.hora_out,
-                    'worked_hours': record.horas_trab,
+                    'check_in': record.check_in,
+                    'check_out': record.check_out,
+                    'worked_hours': record.worked_hours,
                     'notas': record.comen,
                     'us_id': record.user_id.name,
                     'type_resi': record.tipo_resid,

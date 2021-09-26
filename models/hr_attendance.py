a34061d53d674df467d5d4f15dbaad704822dedd
lizeth-miranda <67765437+lizeth-miranda@users.noreply.github.com>
2021-09-25 22:40:15 -0600

    Update hr_attendance.py

--- models/hr_attendance.py
+++ models/hr_attendance.py
@@ -3,6 +3,8 @@
 from odoo import api, fields, models, exceptions, _
 from datetime import date, datetime, timedelta
 from odoo.exceptions import ValidationError, UserError
+# import time
+# import pytz
 
 
 class hr_atten(models.Model):
@@ -14,6 +16,9 @@ class hr_atten(models.Model):
     )
     fecha = fields.Date(string="Fecha Registro",
                         required=True, readonly=False, default=fields.Date.today)
+    Date = fields.Date(
+        compute='_Date',
+        store=True,)
 
     codigo_empleado = fields.Char(
         related="employee_id.pin",
@@ -34,9 +39,6 @@ class hr_atten(models.Model):
         store=True,
     )
 
-    # dia = fields.Selection(
-    #     related="employee_id.resource_calendar_id.attendance_ids.dayofweek",
-    # )
     hours = fields.Float(
         related="employee_id.horas_lab",
         string="Horas Laborales",
@@ -60,31 +62,41 @@ class hr_atten(models.Model):
         # store=True,
         string="Horas Totales"
     )
+
     comen = fields.Char(string="Comentarios",)
 
     user_id = fields.Many2one('res.users', string='Residente',
                               required=False, readonly=True, default=lambda self: self.env.user.id)
-    tipo_resid = fields.Selection(related="user_id.tipo_resi")
+    tipo_resid = fields.Selection(related="user_id.tipo_resi",)
     tipo_empl = fields.Selection(related="employee_id.tipo_emp",)
+
     asistencia = fields.Boolean(default=True, string="asistencia",)
 
     block_lines = fields.Selection([('done', 'Registrado')], string='Estado', )
     tem = fields.Float(string="Temperatura",)
 
-
-    @ api.depends('fecha')
+    @ api.depends('Date')
     def _day(self):
         for record in self:
-            record.day = record.fecha.weekday()
+            record.day = record.Date.weekday()
+    # fecha para agrupar las horas totales de los empleados con la fecha de la
+    # asitencia y no la de la fecha registro
 
+    @api.depends('check_in')
+    def _Date(self):
+        for record in self:
+            dt = record.check_in
+            date = fields.Datetime.to_string(fields.Datetime.context_timestamp(
+                self, fields.Datetime.from_string(dt)))[:10]
+            record.Date = date
 
-    @ api.depends('fecha')
+    @ api.depends('Date')
     def _total_hours(self):
         for attendance in self:
             attendance.total_hours = sum(self.env['hr.attendance'].search([
                 ('employee_id', '=', attendance.employee_id.name),
                 # ('account_ids', '=', attendance.account_ids.id),
-                ('fecha', '=', attendance.fecha),
+                ('Date', '=', attendance.Date),
             ]).mapped('worked_hours'))
 
     @ api.depends('check_out')
@@ -103,7 +115,6 @@ class hr_atten(models.Model):
 
             elif attendance.day == 6 and attendance.tipo_empl != 'admin':
                 attendance.hours_extra = attendance.total_hours
-
 
     # create a new line, as none existed before
 
@@ -135,10 +146,7 @@ class hr_atten(models.Model):
                     'us_id': record.user_id.name,
                     'type_resi': record.tipo_resid,
                     'asis': record.asistencia,
-                    # 'total_extra': record.total_extra,
-                    # 'cost_total': record.cost_total,
-                    # 'total_inci': record.total_inci,
-
+                    
                 })
             record.block_lines = 'done'
         return {

14c2276bdc6e650e1b98374774c9f7b869fdf09d
lizeth-miranda <67765437+lizeth-miranda@users.noreply.github.com>
2021-12-01 16:54:36 -0700

    Update __init__.py

--- models/__init__.py
+++ models/__init__.py
@@ -2,6 +2,7 @@
 from . import hr_employee
 from . import hr_attendance
 from . import hr_leave
+from . import hr_leave_type
 from . import account_analytic
 from . import account_analytic_account
 from . import res_user

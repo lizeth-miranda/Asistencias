649fde0a10815a566c49c63d377a106fdce7a367
lizeth-miranda <67765437+lizeth-miranda@users.noreply.github.com>
2022-06-03 10:56:04 -0600

    Update __init__.py

--- models/__init__.py
+++ models/__init__.py
@@ -6,10 +6,10 @@ from . import hr_leave_type
 from . import account_analytic
 from . import account_analytic_account
 from . import res_user
-from . import nomina
-from . import sale_order
-from . import discounts_employee
-from . import discounts_loans
-from . import quality
-from . import semanas
+#from . import nomina
+#from . import sale_order
+#from . import discounts_employee
+#from . import discounts_loans
+#from . import quality
+#from . import semanas
 
